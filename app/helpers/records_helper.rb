bc6e730f8b87db6c6ba6fd27a0cf81d3d755dc9e
Kelsey Suyehira <k.suyehira@gmail.com>
2015-11-08 20:40:01 -0700

    moving selfData RoR redo to separate file

--- app/helpers/records_helper.rb
+++
@@ -1,9 +0,0 @@
-module RecordsHelper
-
-  def get_task_name(task_id)
-    @user = User.find(params[:user_id])
-    @task = @user.tasks.find(task_id)
-    @task_name = @task.name
-  end
- 
-end
--- app/models/record.rb
+++
@@ -1,6 +0,0 @@
-class Record < ActiveRecord::Base
-  belongs_to :date_object
-  validates_inclusion_of :hours, :in => 0..24, presence: true
-  validates_inclusion_of :minutes, :in => 0..60, presence: true
-  validates :task_id, presence: true
-end
